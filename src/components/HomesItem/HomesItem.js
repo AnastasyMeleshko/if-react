b0d5b792ea76188d7288c766e3f79d4fae24f3ce
AnastasyMeleshko <7634907@mail.ru>
2022-03-21 15:33:37 +0300

    add homes-items

--- src/components/HomesItem/HomesItem.js
+++ src/components/HomesItem/HomesItem.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import './HomesItems.css';
 
-function HomesItem(props) {
+function HomesItem(element) {
    return (
        <div className="homes-item col-lg-3 col-md-4 col-sm-6 col-xs-6">
-
+           <img className="homes-item-img" src={element.item.imageUrl} alt={element.item.name}/>
+           <p className="homes-item-title">{element.item.name}</p>
+           <p className="homes-item-location">{element.item.city}, {element.item.country}</p>
        </div>
    )
 }
 
 export default HomesItem;
-
-
-
-// import '../../views/modifier.css'
-// import './Card.css'
-//
-// function Card(i) {
-//     return <div className='homesCard'>
-//         <img src={i.item.imageUrl} alt="hotel"/>
-//         <p className='-blue-color'>{i.item.name}</p>
-//         <p className='-gray-color'>{`${i.item.city}, ${i.item.country}`}</p>
-//     </div>
-// }
-//
-// export default Card
